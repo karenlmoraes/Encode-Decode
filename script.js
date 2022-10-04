723ec7a7bc1952b6199d6d66fd79b0b6489d2aaf
karenlmoraes <karenlmoraes@gmail.com>
2022-10-04 20:06:47 -0300

    Alteração js (base64 functional)

--- script.js
+++ script.js
@@ -1,76 +1,82 @@
 let selecionar = document.querySelector('.select');
-let chave = document.querySelector('.chave-cont');
-let codificar = document.querySelector('#codificar');
-let decodificar = document.querySelector('#decodificar');
+let chaveCifra = document.getElementById('chaveCont');
+let radioBtn = document.querySelector('.radiobtn');
+let codificar = document.getElementById('codificar');
+let decodificar = document.getElementById('decodificar');
+let resultBtn = document.getElementById('resultbtn');
+let texto = document.getElementById('texto');
+let resultado = document.getElementById('resultado');
 
-// Chave da Cifra de César//
 
-selecionar.addEventListener("click", function () {
-    if(selecionar.value == "cifra") {
-        chave.style.display = "block";
-    } else {
-        chave.style.display = "none";
-    }
-})
-
-// Codificação Base64
-
-function base64() {
-    let texto = document.querySelector("#texto").value;
-
-    if(codificar.checked) {
-        let codificado = btoa(texto);
-        return codificado;
-    } else if(decodificar.checked) {
-        let decodificado = atob(texto);
-        return decodificado;
-    }
+// Somente se Cifra de César for escolhido, aparecerá a chave.
+function selecao() {
+    selecionar.addEventListener("click", function () {
+        if (selecionar.value == "cifra") {
+            chaveCifra.style.display = "block";
+        } else {
+            chaveCifra.style.display = "none";
+        }
+    });
 }
 
-// Codificação Cifra de César
-
-function base64() {
-    let text = document.querySelector("#texto").value;
-    let chave = parseInt(document.querySelector("range").value);
-    let saida = '';
-
-    if(codificar.checked) {
-        for(let i = 0; i < text.length; i++){
-            if(text[i] === text[i].toUpperCase()){
-                saida += String.fromCharCode((text.charCodeAt(i) + chave - 65) % 26 + 65);
-            } else {
-                saida += String.fromCharCode((text.charCodeAt(i) + chave - 97) % 26 + 97);
-            }
-        }
-        return saida;
-    } else if (decodificar.checked) {
-        for(let i = 0; i < text.length; i++){
-            if(text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122 {
-                saida += String.fromCharCode((text.charCodeAt(i) - 97 -  chave + 26) % 26 + 97);
-            } else if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
-                saida += String.fromCharCode((text.charCodeAt(i) - 65 - chave + 26) % 26 + 65);
-            } else {
-                saida += String.fromCharCode(text.charCodeAt(i));
-            }
-        }
-        return saida;
-    }
-}
-
-// Botão
-radiobtn.addEventListener("click", function () {
+// Radio Button e botão de resultado!
+radioBtn.addEventListener("click", function () {
     if (codificar.checked) {
-      btn.innerHTML = "Codificar Mensagem!";
+        resultBtn.innerHTML = "Codificar Mensagem!";
     } else if (decodificar.checked) {
-      btn.innerHTML = "Decodificar Mensagem!";
+        resultBtn.innerText = "Decodificar Mensagem!";
     }
-  });
+});
+
+// Base64 criptography
+function base64() {
+    let texto = document.getElementById('texto').value;
+
+    if (codificar.checked) {
+        let encodeBase64 = btoa(texto)
+        return encodeBase64;
+    } else if (decodificar.checked) {
+        let decodeBase64 = atob(texto)
+        return decodeBase64;
+    }   
+}
+
+// Cifra de César criptography
+function cifraDeCesar() {
+    let texto = document.querySelector('texto').value;
+    let chave = parseInt(document.querySelector('rangenumber').value);
+    let saida = '';
   
-  btn.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (selecione.value === "base64") {
-      resultado.innerText = base64();
-    } else if (selecione.value === "cifra") {
-      resultado.innerText = cifraCesar();
+    if (codificar.checked) {
+      for (let i = 0; i < texto.length; i++) {
+        if (texto[i] === texto[i].toUpperCase()) {
+          saida += String.fromCharCode((texto.charCodeAt(i) + chave - 65) % 26 + 65); 
+        } else {
+          saida += String.fromCharCode((texto.charCodeAt(i) + chave - 97) % 26 + 97);
+        }
+      }
+      return saida;
+  
+    } else if (decodificar.checked) {
+      for (let i = 0; i < texto.length; i++) {
+        if (texto.charCodeAt(i) >= 97 && texto.charCodeAt(i) <= 122) {
+          saida += String.fromCharCode((texto.charCodeAt(i) - 97 -  chave + 26) % 26 + 97);
+        } else if (texto.charCodeAt(i) >= 65 && texto.charCodeAt(i) <= 90) {
+          saida += String.fromCharCode((texto.charCodeAt(i) - 65 - chave + 26) % 26 + 65);
+        } else {
+          saida += String.fromCharCode(texto.charCodeAt(i));
+        }
+      }
+      return saida;
     }
-  });
+  }
+
+// Mostrando o resultado
+resultBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (selecionar.value == "cifra") {
+        resultado.value = cifraDeCesar();
+    } else if (selecionar.value == 'base64') {
+        resultado.value = base64();
+    }
+});
